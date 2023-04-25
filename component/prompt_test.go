b9091f88805b422cd01b7f9c8a66c341fb3cc981
Panchajanya Mysarla <mpanchajanya@vmware.com>
2023-04-25 12:33:37 -0500

    Add validators functionality to prompts

--- component/prompt_test.go
+++ component/prompt_test.go
@@ -4,9 +4,11 @@
 package component
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/AlecAivazis/survey/v2"
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -78,4 +80,97 @@ func Test_PromptOptions(t *testing.T) {
 	assert.Equal("?", options.Icons.Question.Text)
 	assert.Equal("cyan+b", options.Icons.Question.Format)
 	assert.Equal(2, len(opts))
+}
+
+func TestAskPrompt_Validation(t *testing.T) {
+	// Setup mock prompts
+	p := &mockPrompt{
+		answers: []string{"", " ", "t", "very-very-long-name", "ALL_CAPS_NAME", "Test", "test"},
+	}
+
+	var res string
+
+	// Setup PromptOpts validators
+	var promptOpts []PromptOpt
+
+	promptOpts = append(
+		promptOpts,
+		WithValidator(survey.Required),
+		WithValidator(NoOnlySpaces),
+		WithValidator(survey.MinLength(2)),
+		WithValidator(survey.MaxLength(4)),
+		WithValidator(NoUpperCase),
+	)
+
+	// Prepare the surveyOpts
+	var options = &PromptOptions{}
+	for _, opt := range promptOpts {
+		err := opt(options)
+		assert.Nil(t, err)
+	}
+	surveyOpts := translatePromptOpts(options)
+
+	// Trigger the Prompt
+	err := survey.Ask([]*survey.Question{
+		{
+			Prompt: p,
+		},
+	}, &res, surveyOpts...)
+
+	if err != nil {
+		t.Fatalf("Ask() = %v", err)
+	}
+
+	if res != "test" {
+		t.Errorf("answer: %q, want %q", res, "test")
+	}
+	if p.cleanups != 1 {
+		t.Errorf("cleanups: %d, want %d", p.cleanups, 1)
+	}
+
+	if err := p.printedErrors[0].Error(); err != "Value is required" {
+		t.Errorf("printed error 1: %q, want %q", err, "Value is required")
+	}
+	if err := p.printedErrors[1].Error(); err != "value contains only spaces" {
+		t.Errorf("printed error 2: %q, want %q", err, "value contains only spaces")
+	}
+	if err := p.printedErrors[2].Error(); err != "value is too short. Min length is 2" {
+		t.Errorf("printed error 3: %q, want %q", err, "value is too short. Min length is 2")
+	}
+	const maxLen4 = "value is too long. Max length is 4"
+	if err := p.printedErrors[3].Error(); err != maxLen4 {
+		t.Errorf("printed error 2: %q, want %q", err, maxLen4)
+	}
+	if err := p.printedErrors[4].Error(); err != maxLen4 {
+		t.Errorf("printed error 2: %q, want %q", err, maxLen4)
+	}
+	if err := p.printedErrors[5].Error(); err != "value contains uppercase characters" {
+		t.Errorf("printed error 2: %q, want %q", err, "value contains uppercase characters")
+	}
+}
+
+type mockPrompt struct {
+	index         int
+	answers       []string
+	cleanups      int
+	printedErrors []error
+}
+
+func (p *mockPrompt) Prompt(*survey.PromptConfig) (interface{}, error) {
+	if p.index >= len(p.answers) {
+		return nil, errors.New("no valid answers provided")
+	}
+	val := p.answers[p.index]
+	p.index++
+	return val, nil
+}
+
+func (p *mockPrompt) Cleanup(*survey.PromptConfig, interface{}) error {
+	p.cleanups++
+	return nil
+}
+
+func (p *mockPrompt) Error(_ *survey.PromptConfig, err error) error {
+	p.printedErrors = append(p.printedErrors, err)
+	return nil
 }

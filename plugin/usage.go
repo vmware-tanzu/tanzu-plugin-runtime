fe30e54c04e7726f190919b26f6f60fb4b01fa78
Panchajanya Mysarla <mpanchajanya@vmware.com>
2023-09-13 13:43:08 -0500

    refactor the print help

--- plugin/usage.go
+++ plugin/usage.go
@@ -26,112 +26,132 @@ var UsageFunc = func(c *cobra.Command) error {
 // CmdTemplate is the template for plugin commands.
 const CmdTemplate = `{{ printHelp . }}`
 
-//nolint:all
-func printHelp(cmd cobra.Command) string {
-	var output string
-	target := types.StringToTarget(cmd.Annotations["target"])
+// Constants for help text labels
+const (
+	usageStr                = "Usage:"
+	aliasesStr              = "Aliases:"
+	examplesStr             = "Examples:"
+	availableCommandsStr    = "Available Commands:"
+	flagsStr                = "Flags:"
+	globalFlagsStr          = "Global Flags:"
+	additionalHelpTopicsStr = "Additional help topics:"
+)
 
-	output += component.Bold("Usage:") + "\n"
+// Helper to format the usage help section.
+func formatUsageHelpSection(cmd *cobra.Command, target types.Target) string {
+	var output strings.Builder
 
-	// Display usage for commands that are runnable
+	base := "  tanzu "
+
 	if cmd.Runnable() {
 		// For kubernetes, k8s, global, or no target display tanzu command path without target
 		if target == types.TargetK8s || target == types.TargetGlobal || target == types.TargetUnknown {
-			output += "  tanzu " + cmd.UseLine() + "\n"
+			output.WriteString(base + cmd.UseLine() + "\n")
 		}
 
-		// For non global, or no target display tanzu command path with target
+		// For non global, or no target ;display tanzu command path with target
 		if target != types.TargetGlobal && target != types.TargetUnknown {
-			output += "  tanzu " + string(target) + " " + cmd.UseLine() + "\n"
+			output.WriteString(base + string(target) + " " + cmd.UseLine() + "\n")
 		}
 	}
 
-	// Display usage for commands that have sub-commands
 	if cmd.HasAvailableSubCommands() {
 		if cmd.Runnable() {
 			// If the command is both Runnable and has sub-commands, let's insert an empty
 			// line between the usage for the Runnable and the one for the sub-commands
-			output += "\n"
+			output.WriteString("\n")
 		}
 		// For kubernetes, k8s, global, or no target display tanzu command path without target
 		if target == types.TargetK8s || target == types.TargetGlobal || target == types.TargetUnknown {
-			output += "  tanzu " + cmd.CommandPath() + " [command]\n"
+			output.WriteString(base + cmd.CommandPath() + " [command]\n")
 		}
 
 		// For non global, or no target display tanzu command path with target
 		if target != types.TargetGlobal && target != types.TargetUnknown {
-			output += "  tanzu " + string(target) + " " + cmd.CommandPath() + " [command]\n"
+			output.WriteString(base + string(target) + " " + cmd.CommandPath() + " [command]\n")
 		}
 	}
+	return output.String()
+}
 
-	// Display Aliases for the plugin if specified
+// Helper to format the help footer.
+func formatHelpFooter(cmd *cobra.Command, target types.Target) string {
+	var footer strings.Builder
+	if !cmd.HasAvailableSubCommands() {
+		return ""
+	}
+
+	footer.WriteString("\n")
+
+	// For kubernetes, k8s, global, or no target display tanzu command path without target
+	if target == types.TargetK8s || target == types.TargetGlobal || target == types.TargetUnknown {
+		footer.WriteString(`Use "`)
+		if !strings.HasSuffix(cmd.CommandPath(), "tanzu ") {
+			footer.WriteString("tanzu ")
+		}
+		footer.WriteString(cmd.CommandPath() + ` [command] --help" for more information about a command.` + "\n")
+	}
+
+	// For non global, or no target display tanzu command path with target
+	if target != types.TargetGlobal && target != types.TargetUnknown {
+		footer.WriteString(`Use "`)
+		if !strings.HasSuffix(cmd.CommandPath(), "tanzu ") {
+			footer.WriteString("tanzu ")
+		}
+		footer.WriteString(string(target) + " " + cmd.CommandPath() + ` [command] --help" for more information about a command.` + "\n")
+	}
+
+	return footer.String()
+}
+
+func printHelp(cmd *cobra.Command) string {
+	var output strings.Builder
+	target := types.StringToTarget(cmd.Annotations["target"])
+
+	output.WriteString(component.Bold(usageStr) + "\n")
+	output.WriteString(formatUsageHelpSection(cmd, target))
+
 	if len(cmd.Aliases) > 0 {
-		output += "\n" + component.Bold("Aliases:") + "\n"
-		output += "  " + cmd.NameAndAliases() + "\n"
+		output.WriteString("\n" + component.Bold(aliasesStr) + "\n")
+		output.WriteString("  " + cmd.NameAndAliases() + "\n")
 	}
 
-	// Display Examples for the plugin if specified
 	if cmd.HasExample() {
-		output += "\n" + component.Bold("Examples:") + "\n"
-		output += cmd.Example + "\n"
+		output.WriteString("\n" + component.Bold(examplesStr) + "\n")
+		output.WriteString(cmd.Example + "\n")
 	}
 
-	// Display Available Commands for the plugin
 	if cmd.HasAvailableSubCommands() {
-		output += "\n" + component.Bold("Available Commands:") + "\n"
+		output.WriteString("\n" + component.Bold(availableCommandsStr) + "\n")
 		for _, c := range cmd.Commands() {
 			if c.IsAvailableCommand() {
-				output += "  " + component.Rpad(c.Name(), c.NamePadding()) + " " + c.Short + "\n"
+				output.WriteString("  " + component.Rpad(c.Name(), c.NamePadding()) + " " + c.Short + "\n")
 			}
 		}
 	}
 
-	// Display Flags of the plugin
 	if cmd.HasAvailableLocalFlags() {
-		output += "\n" + component.Bold("Flags:") + "\n"
-		output += strings.TrimRight(cmd.LocalFlags().FlagUsages(), " ")
+		output.WriteString("\n" + component.Bold(flagsStr) + "\n")
+		output.WriteString(strings.TrimRight(cmd.LocalFlags().FlagUsages(), " "))
 	}
 
-	// Display Global Flags of the plugin
 	if cmd.HasAvailableInheritedFlags() {
-		output += "\n" + component.Bold("Global Flags:") + "\n"
-		output += strings.TrimRight(cmd.InheritedFlags().FlagUsages(), " ")
+		output.WriteString("\n" + component.Bold(globalFlagsStr) + "\n")
+		output.WriteString(strings.TrimRight(cmd.InheritedFlags().FlagUsages(), " "))
 	}
 
-	// Display Additional help topics of the plugin
 	if cmd.HasHelpSubCommands() {
-		output += "\n" + component.Bold("Additional help topics:") + "\n"
+		output.WriteString("\n" + component.Bold(additionalHelpTopicsStr) + "\n")
 		for _, c := range cmd.Commands() {
 			if c.IsAdditionalHelpTopicCommand() {
-				output += "  " + component.Rpad(c.CommandPath(), c.CommandPathPadding()) + " " + c.Short + "\n"
+				output.WriteString("  " + component.Rpad(c.CommandPath(), c.CommandPathPadding()) + " " + c.Short + "\n")
 			}
 		}
 	}
 
-	// Display a note at the very bottom to indicate how to get more help
-	if cmd.HasAvailableSubCommands() {
-		output += "\n"
+	output.WriteString(formatHelpFooter(cmd, target))
 
-		// For kubernetes, k8s, global, or no target display tanzu command path without target
-		if target == types.TargetK8s || target == types.TargetGlobal || target == types.TargetUnknown {
-			output += `Use "` //nolint:goconst
-			if !strings.HasSuffix(cmd.CommandPath(), "tanzu ") {
-				output += "tanzu " //nolint:goconst
-			}
-			output += cmd.CommandPath() + ` [command] --help" for more information about a command.` + "\n"
-		}
-
-		// For non global, or no target display tanzu command path with target
-		if target != types.TargetGlobal && target != types.TargetUnknown {
-			output += `Use "`
-			if !strings.HasSuffix(cmd.CommandPath(), "tanzu ") {
-				output += "tanzu "
-			}
-			output += string(target) + " " + cmd.CommandPath() + ` [command] --help" for more information about a command.` + "\n"
-		}
-	}
-
-	return output
+	return output.String()
 }
 
 // TemplateFuncs are the template usage funcs.

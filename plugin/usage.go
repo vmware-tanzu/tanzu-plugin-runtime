64f846bb9c6f256caa2e759d90c1d7589ca8856a
Panchajanya Mysarla <mpanchajanya@vmware.com>
2023-09-14 11:38:17 -0500

    Deprecate CmdTemplate and introduce private cmdTemplate variable and update all usages

--- plugin/usage.go
+++ plugin/usage.go
@@ -16,7 +16,7 @@ import (
 
 // UsageFunc is the usage func for a plugin.
 var UsageFunc = func(c *cobra.Command) error {
-	t, err := template.New("usage").Funcs(TemplateFuncs).Parse(CmdTemplate)
+	t, err := template.New("usage").Funcs(TemplateFuncs).Parse(cmdTemplate)
 	if err != nil {
 		return err
 	}
@@ -24,7 +24,33 @@ var UsageFunc = func(c *cobra.Command) error {
 }
 
 // CmdTemplate is the template for plugin commands.
-const CmdTemplate = `{{ printHelp . }}`
+// Deprecated: This variable is deprecated.
+const CmdTemplate = `{{ bold "Usage:" }}
+  {{if .Runnable}}{{ $target := index .Annotations "target" }}{{ if or (eq $target "kubernetes") (eq $target "k8s") }}tanzu {{.UseLine}}{{ end }}{{ if and (ne $target "global") (ne $target "") }}tanzu {{ $target }} {{ else }} {{ end }}{{.UseLine}}{{end}}{{if .HasAvailableSubCommands}}{{ $target := index .Annotations "target" }}{{ if or (eq $target "kubernetes") (eq $target "k8s") }}tanzu {{.CommandPath}} [command]{{end}}{{ if and (ne $target "global") (ne $target "") }}tanzu {{ $target }} {{ else }} {{ end }}{{.CommandPath}} [command]{{end}}{{if gt (len .Aliases) 0}}
+
+{{ bold "Aliases:" }}
+  {{.NameAndAliases}}{{end}}{{if .HasExample}}
+
+{{ bold "Examples:" }}
+  {{.Example}}{{end}}{{if .HasAvailableSubCommands}}
+
+{{ bold "Available Commands:" }}{{range .Commands}}{{if .IsAvailableCommand }}
+  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableLocalFlags}}
+
+{{ bold "Flags:" }}
+{{.LocalFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasAvailableInheritedFlags}}
+
+{{ bold "Global Flags:" }}
+{{.InheritedFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasHelpSubCommands}}
+
+{{ bold "Additional help topics:" }}{{range .Commands}}{{if .IsAdditionalHelpTopicCommand}}
+  {{rpad .CommandPath .CommandPathPadding}} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableSubCommands}}
+
+{{ $target := index .Annotations "target" }}{{ if or (eq $target "kubernetes") (eq $target "k8s") }}Use "{{if beginsWith .CommandPath "tanzu "}}{{.CommandPath}}{{else}}tanzu {{.CommandPath}}{{end}} [command] --help" for more information about a command.{{end}}Use "{{if beginsWith .CommandPath "tanzu "}}{{.CommandPath}}{{else}}tanzu{{ $target := index .Annotations "target" }}{{ if and (ne $target "global") (ne $target "") }} {{ $target }} {{ else }} {{ end }}{{.CommandPath}}{{end}} [command] --help" for more information about a command.{{end}}
+`
+
+// cmdTemplate is the template for plugin commands.
+const cmdTemplate = `{{ printHelp . }}`
 
 // Constants for help text labels
 const (

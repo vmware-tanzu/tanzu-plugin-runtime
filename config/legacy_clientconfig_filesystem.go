e54184b2a027348a5866b054fb3d67a5717aca0f
Panchajanya Mysarla <mpanchajanya@vmware.com>
2023-12-13 23:03:07 +0530

    Update feature flags apis

--- config/legacy_clientconfig_filesystem.go
+++ config/legacy_clientconfig_filesystem.go
@@ -10,19 +10,23 @@ import (
 var (
 	// legacyLocalDirName is the name of the old local directory in which to look for tanzu state. This will be
 	// removed in the future in favor of LocalDirName.
+	// Deprecated: This legacyLocalDirName is deprecated. Use next get config apis
 	legacyLocalDirName = ".tanzu"
 )
 
+// Deprecated: This legacyLocalDir is deprecated. Use next get config apis
 func legacyLocalDir() (path string, err error) {
 	return localDirPath(legacyLocalDirName)
 }
 
 // legacyConfigPath returns the legacy tanzu config path, checking for environment overrides.
+// Deprecated: This legacyConfigPath is deprecated. Use next get config apis
 func legacyConfigPath() (path string, err error) {
 	return legacyCfgPath(legacyLocalDir)
 }
 
 // legacyCfgPath constructs the full config path
+// Deprecated: This legacyCfgPath is deprecated. Use next get config apis
 func legacyCfgPath(localDirGetter func() (string, error)) (path string, err error) {
 	localDir, err := localDirGetter()
 	if err != nil {

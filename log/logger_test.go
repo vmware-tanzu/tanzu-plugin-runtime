d0ea2325dc13afb24ad58957fa12a1b715cf41aa
Panchajanya Mysarla <106101699+mpanchajanya@users.noreply.github.com>
2024-02-02 12:45:20 -0600

    Implement sync threshold values set using env variable TANZU_CLI_LOG_LEVEL (#156)
    * Add new instance of logger for each log type to pick the latest env variables
    
    * Add syncThreshold method to sync the threshold value based on env variable before printing

--- log/logger_test.go
+++ log/logger_test.go
@@ -12,46 +12,52 @@ import (
 )
 
 func TestLogger(t *testing.T) {
-	assert := assert.New(t)
-
 	tests := []struct {
-		test                  string
+		title                 string
+		initialLogLevel       string
 		logLevel              string
 		containStrings        []string
 		doesNotContainStrings []string
 	}{
+
 		{
-			test:                  "when TANZU_CLI_LOG_LEVEL is not configured",
-			logLevel:              "",
-			containStrings:        []string{"log-default", "log-0", "log-1", "log-3"},
-			doesNotContainStrings: []string{"log-5", "log-8"},
-		},
-		{
-			test:                  "when TANZU_CLI_LOG_LEVEL is set to 3",
+			title:                 "when TANZU_CLI_LOG_LEVEL is not configured and then set to 3",
+			initialLogLevel:       "",
 			logLevel:              "3",
 			containStrings:        []string{"log-default", "log-0", "log-1", "log-3"},
 			doesNotContainStrings: []string{"log-5", "log-8"},
 		},
 		{
-			test:                  "when TANZU_CLI_LOG_LEVEL is set to 6",
+			title:                 "when TANZU_CLI_LOG_LEVEL is set to 1 and 5",
+			initialLogLevel:       "1",
+			logLevel:              "5",
+			containStrings:        []string{"log-default", "log-0", "log-1", "log-5"},
+			doesNotContainStrings: []string{"log-3", "log-8"},
+		},
+		{
+			title:                 "when TANZU_CLI_LOG_LEVEL is set to 3 and 6",
+			initialLogLevel:       "3",
 			logLevel:              "6",
 			containStrings:        []string{"log-default", "log-0", "log-1", "log-3", "log-5"},
 			doesNotContainStrings: []string{"log-8"},
 		},
 		{
-			test:                  "when TANZU_CLI_LOG_LEVEL is set to 8",
+			title:                 "when TANZU_CLI_LOG_LEVEL is set to 3 and 8",
+			initialLogLevel:       "3",
 			logLevel:              "8",
 			containStrings:        []string{"log-default", "log-0", "log-1", "log-3", "log-5", "log-8"},
 			doesNotContainStrings: []string{},
 		},
 		{
-			test:                  "when TANZU_CLI_LOG_LEVEL is set to 9",
+			title:                 "when TANZU_CLI_LOG_LEVEL is set to 5 and 9",
+			initialLogLevel:       "5",
 			logLevel:              "9",
 			containStrings:        []string{"log-default", "log-0", "log-1", "log-3", "log-5", "log-8"},
 			doesNotContainStrings: []string{},
 		},
 		{
-			test:                  "when TANZU_CLI_LOG_LEVEL is configured with incorrect value",
+			title:                 "when TANZU_CLI_LOG_LEVEL is configured with incorrect value",
+			initialLogLevel:       "a",
 			logLevel:              "a",
 			containStrings:        []string{"log-default", "log-0", "log-1", "log-3"},
 			doesNotContainStrings: []string{"log-5", "log-8"},
@@ -59,25 +65,31 @@ func TestLogger(t *testing.T) {
 	}
 
 	for _, spec := range tests {
-		defer os.Setenv(EnvTanzuCLILogLevel, "")
-		os.Setenv(EnvTanzuCLILogLevel, spec.logLevel)
+		t.Run(spec.title, func(t *testing.T) {
+			defer os.Setenv(EnvTanzuCLILogLevel, "")
 
-		l = NewLogger()
-		stderr := &bytes.Buffer{}
-		SetStderr(stderr)
+			l = NewLogger()
+			stderr := &bytes.Buffer{}
+			SetStderr(stderr)
 
-		Info("log-default")
-		V(0).Info("log-0")
-		V(1).Info("log-1")
-		V(3).Info("log-3")
-		V(5).Info("log-5")
-		V(8).Info("log-8")
+			os.Setenv(EnvTanzuCLILogLevel, spec.initialLogLevel)
 
-		for _, logStr := range spec.containStrings {
-			assert.Contains(stderr.String(), logStr, spec.test)
-		}
-		for _, logStr := range spec.doesNotContainStrings {
-			assert.NotContains(stderr.String(), logStr, spec.test)
-		}
+			Info("log-default")
+			V(0).Info("log-0")
+			V(1).Info("log-1")
+			V(3).Info("log-3")
+
+			os.Setenv(EnvTanzuCLILogLevel, spec.logLevel)
+
+			V(5).Info("log-5")
+			V(8).Info("log-8")
+
+			for _, logStr := range spec.containStrings {
+				assert.Contains(t, stderr.String(), logStr, spec.title)
+			}
+			for _, logStr := range spec.doesNotContainStrings {
+				assert.NotContains(t, stderr.String(), logStr, spec.title)
+			}
+		})
 	}
 }

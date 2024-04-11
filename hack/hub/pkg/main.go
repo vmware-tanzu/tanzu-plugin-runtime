9e1aa063e490b2e43f74c37fb2e71f0b47919499
Anuj Chaudhari <anujc@vmware.com>
2024-04-11 15:19:59 -0700

    Add support to create authenticated Tanzu Hub client

---
+++ hack/hub/pkg/main.go
@@ -0,0 +1,12 @@
+// Code generated, DO NOT EDIT.
+
+// Package hub used to generate graphQL client code for the graphQL APIs
+package hub
+
+import (
+	// Used for generating client code
+	_ "github.com/Khan/genqlient/generate"
+	_ "github.com/Khan/genqlient/graphql"
+)
+
+//go:generate go run github.com/Khan/genqlient genqlient.yaml

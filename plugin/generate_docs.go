dff25db0f2dbff23682431ffd6158f881a1bff84
Vui Lam <vui.lam@broadcom.com>
2024-04-26 09:27:44 -0700

    Add doc generation that accounts for command mapping
    The approach taken is to recreate the tanzu CLI command tree with only
    the plugin's commands, after accounting for the mapping directives,
    and hand said command tree to cobra's doc generation APIs.
    
    Signed-off-by: Vui Lam <vui.lam@broadcom.com>

--- plugin/generate_docs.go
+++ plugin/generate_docs.go
@@ -7,6 +7,7 @@ package plugin
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strings"
 
 	"github.com/pkg/errors"
@@ -22,35 +23,102 @@ var (
 	docsDir string
 )
 
-func generateDocsBasic(cmd *cobra.Command, args []string) error {
-	if docsDir == "" {
-		docsDir = DefaultDocsDir
+func hierarchyFromPath(cmdPath string) []string {
+	return strings.Fields(cmdPath)
+}
+
+// mapCommand inserts a subcommand at the position of the tanzu command tree as
+// specified by mapEntry, ensuring that any ancestor commands are created as
+// well, if necessary.
+func mapCommand(tanzuCmd, subCommand *cobra.Command, mapEntry *CommandMapEntry) {
+	if mapEntry == nil || mapEntry.DestinationCommandPath == "" {
+		return
 	}
-	if dir, err := os.Stat(docsDir); err != nil || !dir.IsDir() {
-		return errors.Wrap(err, fmt.Sprintf(ErrorDocsOutputFolderNotExists, docsDir))
-	}
-	identity := func(s string) string {
-		if !strings.HasPrefix(s, "tanzu") {
-			return fmt.Sprintf("tanzu_%s", s)
+
+	dstHierarchy := hierarchyFromPath(mapEntry.DestinationCommandPath)
+	numParts := len(dstHierarchy)
+
+	for i := 1; i <= numParts; i++ {
+		cmd, cmdParent := findSubCommandByHierarchy(tanzuCmd, dstHierarchy[:i], matchOnCommandName)
+		if cmdParent == nil {
+			break
 		}
-		return s
-	}
-	emptyStr := func(s string) string { return "" }
 
-	tanzuCmd := cobra.Command{
-		Use: "tanzu",
+		if cmd == nil {
+			// dealing with actual command now
+			if i == numParts {
+				cmd = subCommand
+				cmd.Hidden = false
+				if mapEntry.Description != "" {
+					cmd.Short = mapEntry.Description
+				}
+				if len(mapEntry.Aliases) != 0 {
+					cmd.Aliases = append([]string{dstHierarchy[numParts-1]}, mapEntry.Aliases...)
+				}
+				subCommand.Use = dstHierarchy[numParts-1]
+			} else {
+				// create missing intermediate command
+				cmd = &cobra.Command{
+					Use: dstHierarchy[i-1],
+				}
+			}
+			cmdParent.AddCommand(cmd)
+		}
 	}
-	// Necessary to generate correct output
-	tanzuCmd.AddCommand(cmd.Parent())
-	if err := doc.GenMarkdownTreeCustom(cmd.Parent(), docsDir, emptyStr, identity); err != nil {
-		return fmt.Errorf("error generating docs %q", err)
+}
+
+// rebuildTanzuCommandTree reconstructs the tanzu CLI's placement of the
+// plugin's commands after accounting for the plugin descriptor's CommandMap.
+// It is primarily used to prep a Command tree for cobra's doc generation APIs
+// warning: commands will be mutated as a side effect
+func rebuildTanzuCommandTree(tanzuCmd, pluginRootCmd *cobra.Command, desc *PluginDescriptor) {
+	tanzuCmd.AddCommand(pluginRootCmd)
+
+	// straightfoward when no command mapping to account for
+	if desc == nil || len(desc.CommandMap) == 0 {
+		return
 	}
 
-	return nil
+	// reconstruct command tree by relocating all mapped commands, then
+	cmap := desc.CommandMap
+	sort.Slice(cmap, func(i, j int) bool { return cmap[i].SourceCommandPath > cmap[j].SourceCommandPath })
+
+	for _, v := range cmap {
+		mapEntry := v
+		cmd, _ := findSubCommandByHierarchy(pluginRootCmd, hierarchyFromPath(mapEntry.SourceCommandPath), matchOnCommandName)
+		mapCommand(tanzuCmd, cmd, &mapEntry)
+	}
 }
 
 func getGenDocFn(desc *PluginDescriptor) func(cmd *cobra.Command, args []string) error {
-	return generateDocsBasic
+	return func(cmd *cobra.Command, args []string) error {
+		if docsDir == "" {
+			docsDir = DefaultDocsDir
+		}
+		if dir, err := os.Stat(docsDir); err != nil || !dir.IsDir() {
+			return errors.Wrap(err, fmt.Sprintf(ErrorDocsOutputFolderNotExists, docsDir))
+		}
+		identity := func(s string) string {
+			if !strings.HasPrefix(s, "tanzu") {
+				return fmt.Sprintf("tanzu_%s", s)
+			}
+			return s
+		}
+		emptyStr := func(s string) string { return "" }
+
+		tanzuCmd := cobra.Command{
+			Use:   "tanzu",
+			Short: "The main Tanzu CLI",
+		}
+
+		rebuildTanzuCommandTree(&tanzuCmd, cmd.Parent(), desc)
+
+		if err := doc.GenMarkdownTreeCustom(&tanzuCmd, docsDir, emptyStr, identity); err != nil {
+			return fmt.Errorf("error generating docs %q", err)
+		}
+
+		return nil
+	}
 }
 
 func newGenDocsCmd(desc *PluginDescriptor) *cobra.Command {

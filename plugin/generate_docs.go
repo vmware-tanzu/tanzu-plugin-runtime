bdc81ee5e492255ada07590a0302278558019ad8
Vui Lam <vui.lam@broadcom.com>
2024-04-25 22:11:52 -0700

    Refactor genDocsCmd, use generator
    Some refactoring to use a generator to produce the doc generation
    function, in preparation for subsequent enhancement to generate docs
    based on command mapping information.
    
    Signed-off-by: Vui Lam <vui.lam@broadcom.com>

--- plugin/generate_docs.go
+++ plugin/generate_docs.go
@@ -22,38 +22,45 @@ var (
 	docsDir string
 )
 
-func init() {
-	genDocsCmd.Flags().StringVarP(&docsDir, "docs-dir", "d", DefaultDocsDir, "destination for docs output")
+func generateDocsBasic(cmd *cobra.Command, args []string) error {
+	if docsDir == "" {
+		docsDir = DefaultDocsDir
+	}
+	if dir, err := os.Stat(docsDir); err != nil || !dir.IsDir() {
+		return errors.Wrap(err, fmt.Sprintf(ErrorDocsOutputFolderNotExists, docsDir))
+	}
+	identity := func(s string) string {
+		if !strings.HasPrefix(s, "tanzu") {
+			return fmt.Sprintf("tanzu_%s", s)
+		}
+		return s
+	}
+	emptyStr := func(s string) string { return "" }
+
+	tanzuCmd := cobra.Command{
+		Use: "tanzu",
+	}
+	// Necessary to generate correct output
+	tanzuCmd.AddCommand(cmd.Parent())
+	if err := doc.GenMarkdownTreeCustom(cmd.Parent(), docsDir, emptyStr, identity); err != nil {
+		return fmt.Errorf("error generating docs %q", err)
+	}
+
+	return nil
 }
 
-var genDocsCmd = &cobra.Command{
-	Use:    "generate-docs",
-	Short:  "Generate Cobra CLI docs for all subcommands",
-	Hidden: true,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		if docsDir == "" {
-			docsDir = DefaultDocsDir
-		}
-		if dir, err := os.Stat(docsDir); err != nil || !dir.IsDir() {
-			return errors.Wrap(err, fmt.Sprintf(ErrorDocsOutputFolderNotExists, docsDir))
-		}
-		identity := func(s string) string {
-			if !strings.HasPrefix(s, "tanzu") {
-				return fmt.Sprintf("tanzu_%s", s)
-			}
-			return s
-		}
-		emptyStr := func(s string) string { return "" }
+func getGenDocFn(desc *PluginDescriptor) func(cmd *cobra.Command, args []string) error {
+	return generateDocsBasic
+}
 
-		tanzuCmd := cobra.Command{
-			Use: "tanzu",
-		}
-		// Necessary to generate correct output
-		tanzuCmd.AddCommand(cmd.Parent())
-		if err := doc.GenMarkdownTreeCustom(cmd.Parent(), docsDir, emptyStr, identity); err != nil {
-			return fmt.Errorf("error generating docs %q", err)
-		}
+func newGenDocsCmd(desc *PluginDescriptor) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:    "generate-docs",
+		Short:  "Generate Cobra CLI docs for all subcommands",
+		Hidden: true,
+		RunE:   getGenDocFn(desc),
+	}
+	cmd.Flags().StringVarP(&docsDir, "docs-dir", "d", DefaultDocsDir, "destination for docs output")
 
-		return nil
-	},
+	return cmd
 }

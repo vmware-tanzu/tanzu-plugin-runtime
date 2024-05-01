0344e49c05fc5f40194897a026a9a6780144fb2a
Anuj Chaudhari <anujc@vmware.com>
2024-05-01 12:24:14 -0700

    Fix tanzuhub.mk file

--- hack/hub/tanzuhub.mk
+++ hack/hub/tanzuhub.mk
@@ -13,11 +13,11 @@ TANZU_HUB_SCHEMA_FILE_URL ?=
 tanzu-hub-stub-init: # Initialize a Tanzu Hub package with stub, schema.graphql and queries.graphql to generate graphQL client APIs
 	mkdir -p $(TANZU_HUB_PKG_DIR)
 	for filename in $(TANZU_HUB_INIT_FILES); do \
-		[ -f $(TANZU_HUB_PKG_DIR)/$$filename ] && echo "Skipping $(TANZU_HUB_PKG_DIR)/$$filename (already exists)" || wget -O "$(TANZU_HUB_PKG_DIR)/$(filename)" $(TANZU_HUB_INIT_PKG_URL)/$$filename ; \
+		[ -f $(TANZU_HUB_PKG_DIR)/$$filename ] && echo "Skipping $(TANZU_HUB_PKG_DIR)/$$filename (already exists)" || wget -O "$(TANZU_HUB_PKG_DIR)/$$filename" $(TANZU_HUB_INIT_PKG_URL)/$$filename ; \
 	done
 	wget -O "$(TANZU_HUB_PKG_DIR)/schema.graphql" $(TANZU_HUB_SCHEMA_FILE_URL)
 	go mod tidy
 
 .PHONY: tanzu-hub-stub-generate
 tanzu-hub-stub-generate: ## Generate golang stub for the Tanzu Hub queries specified under $(TANZU_HUB_PKG_BASE_DIR)/hub/queries.graphql
-	go generate ./$(TANZU_HUB_PKG_DIR)/...
+	go generate $(TANZU_HUB_PKG_DIR)/...
